1ae007d4a5a14edfd1320f023166c8669fc6dd13
jeshiihu <jeshiihu@gmail.com>
2017-03-31 09:34:08 -0600

    mapping to get an array of ints

--- task1.scala
+++ task1.scala
@@ -3,13 +3,7 @@ val dataInput = sc.textFile("data_input")
 val lCount = dataInput.count()
 val iCount = lCount.toInt
 
+// obtaining an array[int] from the data
 val arrInput = dataInput.take(iCount)
-
-val line = arrInput(0);
-val split = line.split("\t")
-val why = split(0).toInt
-val idk = split(1).toInt
-
-for( x <- arrInput ){
-   val split = x.split(" ")
-}
+val splitInput = arrInput.map(_.split("\t"))
+val intInput = splitInput.map(_.map(_.toInt))
